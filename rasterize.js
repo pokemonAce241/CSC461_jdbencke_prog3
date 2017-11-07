7c854950f43eace8983b60952c5cd7f4681bf002
jdbencke <jdbencke@ncsu.edu>
2017-11-06 19:59:31 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -238,6 +238,18 @@ function setupWebGL() {
     
     // Set up keys
     document.onkeydown = handleKeyDown; // call this when key pressed
+    
+     // Get the image canvas, render an image in it
+     var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
+      var cw = imageCanvas.width, ch = imageCanvas.height; 
+      imageContext = imageCanvas.getContext("2d"); 
+      var bkgdImage = new Image(); 
+      bkgdImage.src = "https://ncsucgclass.github.io/prog3/sky.jpg";
+      bkgdImage.onload = function(){
+          var iw = bkgdImage.width, ih = bkgdImage.height;
+          imageContext.drawImage(bkgdImage,0,0,iw,ih,0,0,cw,ch);   
+     } // end onload callback
+
 
     // Get the canvas and context
     var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
